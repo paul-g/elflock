d4935a025d1965cb333299cd9e1e684da51501bc
paul-g <paul.grigoras90@gmail.com>
2012-08-31 00:18:25 +0100

    Add total count, positive & negative to aggregated records.

--- src/org/paulg/ispend/main/AggregatedRecord.java
+++ src/org/paulg/ispend/main/AggregatedRecord.java
@@ -4,16 +4,21 @@ public class AggregatedRecord {
 
 	private String description;
 	private double value;
+	private int count;
+	private double positive;
+	private double negative;
 
 	public AggregatedRecord(final Record r) {
 		super();
 		description = r.getDescription();
 		value = r.getValue();
+		count = 0;
 	}
 
 	public AggregatedRecord(final String description, final double value) {
 		this.description = description;
 		this.value = value;
+		count = 0;
 	}
 
 	public String getDescription() {
@@ -34,6 +39,36 @@ public class AggregatedRecord {
 
 	public void addRecord(final Record r) {
 		value += r.getValue();
+		count++;
+		if (r.getValue() < 0) {
+			negative += r.getValue();
+		} else {
+			positive += r.getValue();
+		}
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(final int count) {
+		this.count = count;
+	}
+
+	public double getPositive() {
+		return positive;
+	}
+
+	public void setPositive(final double positive) {
+		this.positive = positive;
+	}
+
+	public double getNegative() {
+		return negative;
+	}
+
+	public void setNegative(final double negative) {
+		this.negative = negative;
 	}
 
 }
