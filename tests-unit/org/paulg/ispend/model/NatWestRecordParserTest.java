20e952a4188981d2e1092ed6dbef4eb611854893
paul-g <paul.grigoras90@gmail.com>
2012-09-02 22:10:58 +0100

    Update tests.

---
+++ tests-unit/org/paulg/ispend/model/NatWestRecordParserTest.java
@@ -0,0 +1,19 @@
+package org.paulg.ispend.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NatWestRecordParserTest {
+
+	private final static String record = "29/03/2011,POS,\"'8854 28MAR11 , MARGARET MILL , LONDON GB\",-9.00,6.07,\"'curent\",\"'515001-64829197\",";
+
+	@Test
+	public void test() {
+		final RecordParser rp = new NatWestRecordParser();
+		final Record r = rp.parseRecord(record);
+		final Record expected = new Record("29/03/2011", "POS", "8854 28MAR11 , MARGARET MILL , LONDON GB", "6.07",
+				"curent", "515001-64829197", -9.00);
+		assertEquals("Constructed record matches parsed record", expected, r);
+	}
+}
--- tests-unit/org/paulg/ispend/utils/StringUtilsTest.java
+++ tests-unit/org/paulg/ispend/utils/StringUtilsTest.java
@@ -1,6 +1,6 @@
 package org.paulg.ispend.utils;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -8,7 +8,15 @@ public class StringUtilsTest {
 
 	@Test
 	public void testContainsIgnoreCase() {
-		assertTrue(StringUtils.containsIgnoreCase("Testabc", "tab"));
-		assertTrue(!StringUtils.containsIgnoreCase("testabc", "cd"));
+		assertTrue("Pattern contained in string", StringUtils.containsIgnoreCase("Testabc", "tab"));
+		assertFalse("Pattern not contained in string", StringUtils.containsIgnoreCase("testabc", "cd"));
+	}
+
+	@Test
+	public void testTrimCharacters() {
+		assertEquals("Trim trailing and leading characters", "abc", StringUtils.trimCharacters("\"abc'", "\"'"));
+		assertEquals("Trim leading character", "abc", StringUtils.trimCharacters("\"abc", "\"'"));
+		assertEquals("Nothing to trim", "abc", StringUtils.trimCharacters("abc", "\"'"));
+		assertEquals("Trim to empty string", "", StringUtils.trimCharacters("'\"'", "\"'"));
 	}
 }
