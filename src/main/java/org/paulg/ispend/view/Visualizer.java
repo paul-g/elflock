c14068687d64ff24fa678c54358c64727cf236d6
Paul Grigoras <paul.grigoras90@gmail.com>
2014-01-17 00:04:57 +0000

    Add a dummy line chart on the historical tab.

--- src/main/java/org/paulg/ispend/view/Visualizer.java
+++ src/main/java/org/paulg/ispend/view/Visualizer.java
@@ -4,7 +4,10 @@ import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.GridPane;
@@ -15,16 +18,48 @@ public class Visualizer extends TabPane {
 
     Visualizer(ObservableList<PieChart.Data> pieChartNegData,
                ObservableList<PieChart.Data> pieChartPosData) {
+        getTabs().add(makeTotalTab(pieChartNegData, pieChartPosData));
+        getTabs().add(makeHistoricalTab());
+
+
+    }
+
+    private Tab makeHistoricalTab() {
         Tab tab = new Tab();
-        tab.setText("Total");
+        tab.setText("Historical");
+
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Number of Month");
+        //creating the chart
+        final LineChart<Number,Number> lineChart =
+                new LineChart<Number,Number>(xAxis,yAxis);
+
+        lineChart.setTitle("History");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("My portfolio");
+        //populating the series with data
+        series.getData().add(new XYChart.Data(1, 1));
+        series.getData().add(new XYChart.Data(2, 2));
+        series.getData().add(new XYChart.Data(3, 3));
+        series.getData().add(new XYChart.Data(4, 4));
+        lineChart.getData().add(series);
+
+        tab.setContent(lineChart);
+        return tab;
+    }
+
+    private Tab makeTotalTab(ObservableList<PieChart.Data> pieChartNegData, ObservableList<PieChart.Data> pieChartPosData) {
+        Tab totalTab = new Tab();
+        totalTab.setText("Total");
 
         HBox box = new HBox();
         Node posChart = pieChart("Income", pieChartPosData, 1, 3);
         Node negChart = pieChart("Expenses", pieChartNegData, 2, 3);
         box.getChildren().addAll(posChart, negChart);
-
-        tab.setContent(box);
-        getTabs().add(tab);
+        totalTab.setContent(box);
+        return totalTab;
     }
 
     private Node pieChart(final String title, final ObservableList<PieChart.Data> data, final int row, final int col) {
