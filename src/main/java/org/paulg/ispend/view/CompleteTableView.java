3cad43d3e34f429be9a9a5b83fe1421eaa522690
Paul Grigoras <paul.grigoras90@gmail.com>
2014-03-01 13:35:30 +0000

    Fix layout.

--- src/main/java/org/paulg/ispend/view/CompleteTableView.java
+++ src/main/java/org/paulg/ispend/view/CompleteTableView.java
@@ -1,13 +1,16 @@
 package org.paulg.ispend.view;
 
-import java.lang.reflect.Field;
-import java.util.*;
-import javafx.scene.control.*;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Table view that binds to all fields (public, private and protected) of a provided class.
- **/
+ */
 class CompleteTableView<T> extends TableView<T> {
 
     public CompleteTableView(final Class<T> classToBind) {
@@ -15,7 +18,9 @@ class CompleteTableView<T> extends TableView<T> {
         getColumns().addAll(columns);
     }
 
-    /** Bind to all declared fields of the provided class. **/
+    /**
+     * Bind to all declared fields of the provided class. *
+     */
     private List<TableColumn<T, String>> makeColumns(final Class<T> clazz) {
         final List<TableColumn<T, String>> columns = new ArrayList<>();
         for (final Field f : clazz.getDeclaredFields()) {
--- src/main/java/org/paulg/ispend/view/GroupView.java
+++ src/main/java/org/paulg/ispend/view/GroupView.java
@@ -1,6 +1,5 @@
 package org.paulg.ispend.view;
 
-import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
