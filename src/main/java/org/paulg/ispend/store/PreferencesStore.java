f11d0696827eea8befa9e315765eee04855c1545
Paul Grigoras <paul.grigoras90@gmail.com>
2014-11-30 01:15:33 +0000

    Also save session data to a json file.

--- src/main/java/org/paulg/ispend/store/PreferencesStore.java
+++ src/main/java/org/paulg/ispend/store/PreferencesStore.java
@@ -1,11 +1,17 @@
 package org.paulg.ispend.store;
 
+import com.google.gson.Gson;
 import org.paulg.ispend.view.dashboard.BudgetEntry;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.prefs.Preferences;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -15,21 +21,29 @@ import static java.util.stream.Collectors.*;
 
 public class PreferencesStore {
 
-    public static final String NO_FILE = "NoFile";
     private static final String LOADED_FILE = "LoadedFile";
     private static final String SAVED_QUERY = "SavedQuery";
     private static final String SAVED_SEARCH_QUERIES = "SavedSearchQueries";
     private static final String SAVED_LABELS = "SavedLabels";
 
-    private final Preferences prefs;
-    private File workspace;
+    private Map<String, List<String>> valuesMap = new HashMap<>();
+
+    private transient final Preferences prefs;
+    private transient String workspace;
 
     public PreferencesStore() {
         prefs = Preferences.userRoot().node(Preferences.class.getName());
+        this.workspace = prefs.get("workspace", null);
     }
 
-    private void saveValues(Stream<String> values, String field) {
-        prefs.put(field, values.collect(joining(";")));
+    private void saveValues(List<String> values, String field) {
+        valuesMap.put(field, values);
+        prefs.put(field, values.stream().collect(joining(";")));
+        try {
+            save();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean hasSavedQueries() {
@@ -83,15 +97,24 @@ public class PreferencesStore {
     }
 
     public void saveBudgetEntries(List<BudgetEntry> budgets) {
-        saveValues(budgets.stream().map(BudgetEntry::getGroup), SAVED_SEARCH_QUERIES);
-        saveValues(budgets.stream().map(BudgetEntry::getLabel), SAVED_LABELS);
+        saveValues(budgets.stream().map(BudgetEntry::getGroup).collect(toList()), SAVED_SEARCH_QUERIES);
+        saveValues(budgets.stream().map(BudgetEntry::getLabel).collect(toList()), SAVED_LABELS);
     }
 
     public String getWorkspace() {
-        return prefs.get("workspace", null);
+        return workspace;
     }
 
     public void saveWorkspace(String absolutePath) {
+        this.workspace = absolutePath;
         prefs.put("workspace", absolutePath);
+    }
+
+    void save() throws IOException {
+        Gson gson = new Gson();
+        Path p = Paths.get(workspace, "config.json");
+        BufferedWriter br = Files.newBufferedWriter(p, Charset.defaultCharset());
+        br.write(gson.toJson(this));
+        br.close();
     }
 }
